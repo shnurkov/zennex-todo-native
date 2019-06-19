440b731f969f2a52c3358d3b56e4b5dbfa2acd18
ssa <sema@Mac-mini-Admin.local>
2019-06-19 16:17:12 +0700

    add functionality task-desc

--- components/task-description.js
+++ components/task-description.js
@@ -8,6 +8,7 @@ const styles = StyleSheet.create({
   container: {
     padding: 15,
     flex: 1,
+    minHeight: Dimensions.get('window').height,
     position: "relative"
   },
   label: {
@@ -16,7 +17,9 @@ const styles = StyleSheet.create({
   input: {
     borderWidth: 1,
     borderColor: "#40A9FF",
-    borderRadius: 5
+    borderRadius: 5,
+    paddingLeft: 15,
+    paddingRight: 15
   },
   row: {
     marginBottom: 20
@@ -27,7 +30,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignSelf: "stretch",
     position: "absolute",
-    bottom: 40,
+    bottom: 50,
     width: Dimensions.get('window').width
   },
   btnOk: {
@@ -54,21 +57,26 @@ export default class TaskDesc extends Component{
       <Modal
           animationType="slide"
           transparent={false}
-          visible={true}
+          visible={!!task}
           onRequestClose={() => {
             this.props.close();
       }}>
+      {task &&
         <View style={styles.container}>
-          {/* {task && */}
+          
           <View style={styles.row}>
             <Text style={styles.label}>Name</Text>
             <TextInput 
+              value = {task.title}
               style ={styles.input}
+              onChangeText = {this.handleTitleChange}
             />
           </View>
           <View style={styles.row}>
             <Text style={styles.label}>Description</Text>
             <TextInput 
+              value = {task.description}
+              onChangeText = {this.handleDescChange}
               style ={styles.input}
               multiline = {true}
               numberOfLines = {4}
@@ -78,15 +86,15 @@ export default class TaskDesc extends Component{
             <Text style={styles.label}>Importance</Text>
             <SegmentedControlTab
               values={['Usual', 'Important', 'Very important']}
-              selectedIndex={this.state.selectedIndex}
-              onTabPress={this.handleIndexChange}
+              selectedIndex={this.getImportanceIndex(task.importance)}
+              onTabPress={this.handleImportanceChange}
             />
           </View>
           <View style={styles.row}>
             <Text style={styles.label}>Date end</Text>
             <DatePicker
               style={{width: Dimensions.get('window').width - 30}}
-              date={this.state.date}
+              date={task.time.end}
               mode="datetime"
               placeholder="Select date"
               format="DD.MM.YYYY HH:mm"
@@ -99,15 +107,16 @@ export default class TaskDesc extends Component{
                   borderRadius: 5
                 }
               }}
-              onDateChange={(date) => {this.setState({date: date})}}
+              onDateChange={this.handleDateChange}
             />
           </View>
+          {task.isDone &&
           <View style={styles.row}>
             <Text style={styles.label}>Date done</Text>
             <DatePicker
               disabled
               style={{width: Dimensions.get('window').width - 30}}
-              date={this.state.date}
+              date={task.time.done}
               mode="datetime"
               placeholder="Select date"
               format="DD.MM.YYYY HH:mm"
@@ -123,7 +132,7 @@ export default class TaskDesc extends Component{
               // onDateChange={(date) => {this.setState({date: date})}}
             />
 
-          </View>
+          </View>}
           <View style={styles.btnWrap}>
             <View style={styles.btnOk}>
             <Button
@@ -145,15 +154,132 @@ export default class TaskDesc extends Component{
             />
             </View>
           </View>
-          {/* } */}
+        
         </View>
+      }
       </Modal>
     );
   }
-  handleIndexChange = (index) => {
-    this.setState({
-      ...this.state,
-      selectedIndex: index,
-    });
+  handleImportanceChange = (index) => {
+    let val;
+    switch(index){
+      case 0:
+        val = "usual";
+        break;
+      case 1:
+        val = "important"
+        break;
+      case 2:
+        val = "very-important";
+        break;
+      default:
+        val = "usual";
+    }
+    this.props.edit(this.props.task.id, {importance: val});
   }
+  getImportanceIndex = (importance) => {
+    let i;
+    switch(importance){
+      case "usual":
+        i = 0;
+        break;
+      case "important":
+        i = 1
+        break;
+      case "very-important":
+        i = 2;
+        break;
+      default:
+        i = 0;
+    }
+    return i;
+  }
+
+  // componentDidUpdate(prevProps){
+  //   if(this.props.task && prevProps.task && this.props.task.id !== prevProps.task.id){
+  //     if(this.props.task.time.end){
+  //       this.setState({
+  //         date: this.props.task.time.end
+  //       })
+  //     }else{
+  //       this.setState({date: ""});
+  //     }
+  //   }
+  // }
+
+
+
+  // disabledStartDate = startValue => {
+  //   let now = new Date();
+  //   now.setHours(0, 0, 0);
+  //   return startValue.valueOf() < now;
+  // }
+
+  // disableHours = () => {
+  //   if(!this.state.isToday) return;
+  //   let hours = [];
+  //   for(let i =0; i < moment().hour(); i++){
+  //       hours.push(i);
+  //   }
+  //   return hours;
+  // }
+
+  // disableMinutes = (selectedHour) => {
+  //   let minutes= [];
+  //   if (selectedHour === moment().hour()){
+  //       for(let i =0; i < moment().minute()+1; i++){
+  //           minutes.push(i);
+  //       }
+  //   }
+  //   return minutes;
+  // }
+
+  handleTitleChange = (val) => {
+    this.props.edit(this.props.task.id, {title: val});
+  }
+
+  handleDescChange = (val) => {
+    this.props.edit(this.props.task.id, {description: val});
+  }
+
+  // handleImportanceChange = (e) => {
+  //   let val = e.target.value;
+  //   this.props.edit(this.props.task.id, {importance: val});
+  // }
+
+  handleDateChange = (_date, dateStr) => {
+    // let dateStr = null;
+    // if(date){
+    //   dateStr = date._d;
+    // }else{
+    //   this.setState({date: dateStr});
+    //   this.props.edit(this.props.task.id, {time: dateStr});
+    //   return;
+    // }
+    this.props.edit(this.props.task.id, {time: dateStr});
+  }
+
+  // handleTimeChange = (_time, timeString) => {
+  //   let t = timeString.split(":");
+  //   let h = +t[0], m = +t[1];
+
+  //   let date = this.state.date;
+  //   let now = new Date();
+
+  //   if(this.state.isToday && h === now.getHours() && m === 0){
+  //     m = now.getMinutes() + 1;
+  //   }
+
+  //   date.setHours(h, m);
+  //   this.setState({date});
+
+  //   this.props.edit(this.props.task.id, {time: date});
+  // }
+
+  // handleDeleteTask = () => {
+  //   this.props.del(this.props.task.id);
+  // }
+  // handleCloseDesc = () => {
+  //   this.props.close();
+  // }
 }
